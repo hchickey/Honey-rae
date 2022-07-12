449c412ccc9ede2e1602759453e2cd3eae64f520
Haley Hickey <hchickey96@gmail.com>
2022-07-12 16:58:00 -0500

    emergency button

---
+++ src/components/tickets/TicketList.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react"
+import "./tickets.css"
+
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFiltered] = useState([])
+    const [emergency, setEmergency] = useState(false)
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+
+    useEffect(
+        () => {
+            if (emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFiltered(emergencyTickets)
+            }
+        },
+        [emergency]
+    )
+
+    useEffect(
+        () => {
+            fetch('http://localhost:8088/serviceTickets')
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+                })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+
+    useEffect(
+        () => {
+            if (honeyUserObject.staff) {
+                // For employees
+                setFiltered(tickets)
+            }
+            else {
+                // For customers
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+        },
+        [tickets]
+    )
+
+
+    return <>
+        <button
+            onClick={
+                () => {
+                    setEmergency(true)
+                }
+            }
+            >Emergency Only</button>
+    <h2>List of Tickets</h2>
+
+    <article className="tickets">
+        {
+            filteredTickets.map(
+                (ticket) => {
+                    return <section className="ticket" key={'ticket--${ticket.id}'}>
+                        <header>{ticket.description}</header>
+                        <footer>Emergency: {ticket.emergency ? "🧨" : "no"}</footer>
+                    </section>
+                }
+            )
+        }
+        
+        </article>    
+        </>
+}
